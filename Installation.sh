5acb7f46e19fa996cc63ac10a38ecb425e3bd6e2
Prashant Mahamuni <mahamuniprashant26@gmail.com>
2025-11-12 15:50:16 +0530

    Added Jenkins installation script

---
+++ Installation.sh
@@ -0,0 +1,16 @@
+sudo apt update
+
+sudo apt install openjdk-17-jdk -y
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key  # downloads jenkins.io.key to access jenkins server 
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \ 
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null  # adds jenkins repo in apt repository 
+
+sudo apt update
+
+sudo apt install jenkins
+
+sudo systemctl enable jenkins 
+
